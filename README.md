1d4e58493f9065e957f6cde0b772e635709ce87a
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-02-28 15:47:27 -0800

    added OHLC data aggregation and news aggregation

---
+++ README.md
@@ -0,0 +1,27 @@
+Steps to Run:
+
+1. set up and activate virtual Environment
+must use python 3.11
+```
+rmdir -r .venv
+py -3.11 -m venv .venv
+.\.venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install yfinance pandas numpy scikit-learn
+```
+
+2. Install required packages
+```
+pip install -r requirements.txt
+```
+
+3. Prepare a FinnHub API key
+```
+create a file called ".env"
+put the API key under .env as <FINHUB_API_KEY="YOUR_API_KEY">
+```
+
+4. Run Aggregator.py to test the connection of everything
+```
+python aggregator.py
+```
--- aggregator.py
+++ aggregator.py
@@ -1,6 +1,26 @@
 import yfinance as yf
 
-ticker = "AAPL"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env file
+#database_url = os.getenv("DATABASE_URL")
+FINNHUB_API = os.getenv("FINHUB_API_KEY")
+
+ticker = "AMZN"
 df = yf.download(ticker, period="5y", interval="1d")
 
 print(df.head())
+
+import finnhub
+from datetime import date, timedelta
+
+client = finnhub.Client(api_key=FINNHUB_API)
+
+symbol = "AMZN"
+to_ = date.today().isoformat()
+from_ = (date.today() - timedelta(days=7)).isoformat()
+
+news = client.company_news(symbol, _from=from_, to=to_)  # _from is required name
+# news is a list of dicts: headline, source, url, datetime, summary, etc.
+print(news[:3])
